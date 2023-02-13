e0379b3b0fc39ca84d99968c1dbc0e2a9f102bfe
AnnemariaRe <annemariarepenko@gmail.com>
2023-02-13 13:34:12 +0300

    fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ async function bootstrap() {
   );
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
 
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);

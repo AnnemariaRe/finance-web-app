6f7622ed006c1946dd3c6a1e2f0fec840fe2d352
AnnemariaRe <annemariarepenko@gmail.com>
2023-03-08 11:00:37 +0300

    feat: connect Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ResponseTimeInterceptor } from './response-time.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import hbs = require('hbs');
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -16,6 +18,20 @@ async function bootstrap() {
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
 
   app.useGlobalInterceptors(new ResponseTimeInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('WalletKeeper Web App')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .addTag('account')
+    .addTag('transaction')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);

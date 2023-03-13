68106f4d7ca35e1b09e76a32cdff62f97c3624ac
AnnemariaRe <annemariarepenko@gmail.com>
2023-03-13 14:23:19 +0300

    refactor: new db table

--- prisma/migrations/20230313111740_init/migration.sql
+++ prisma/migrations/20230313111740_init/migration.sql
@@ -4,9 +4,6 @@ CREATE TYPE "AccountType" AS ENUM ('current', 'savings', 'credit', 'deposit');
 -- CreateEnum
 CREATE TYPE "OperationType" AS ENUM ('expense', 'income');
 
--- CreateEnum
-CREATE TYPE "Category" AS ENUM ('grocery', 'restaurants', 'transport', 'housing', 'medicine', 'travel');
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
@@ -38,11 +35,22 @@ CREATE TABLE "Transaction" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "operationType" "OperationType" NOT NULL,
     "amount" DECIMAL(65,30) NOT NULL,
-    "category" "Category" NOT NULL,
+    "categoryId" INTEGER NOT NULL,
     "accountId" INTEGER NOT NULL,
     "transactionDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "operationType" "OperationType" NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -52,4 +60,10 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

31f3bf37190cfff267e4f09606ab6cefe197c21e
AnnemariaRe <annemariarepenko@gmail.com>
2023-03-27 13:32:45 +0300

    refactor: remove currency create method

--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,20 +1,12 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CurrenciesService } from './currencies.service';
-import { CreateCurrencyDto } from './dto/create-currency.dto';
 import { Currency } from './entities/currency.entity';
 
 @Controller('currencies')
 @ApiTags('currency')
 export class CurrenciesController {
   constructor(private readonly currenciesService: CurrenciesService) {}
-
-  @ApiOperation({summary: 'Create currency'})
-  @Post()
-  @ApiCreatedResponse({ type: Currency })
-  create(@Body() createCurrencyDto: CreateCurrencyDto) : Promise<Currency> {
-    return this.currenciesService.create(createCurrencyDto);
-  }
 
   @ApiOperation({summary: 'Get all currencies'})
   @Get()
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateCurrencyDto } from './dto/create-currency.dto';
 import { Currency } from './entities/currency.entity';
 
 @Injectable()
@@ -10,18 +9,6 @@ export class CurrenciesService {
     @InjectRepository(Currency)
     private readonly currencyRepository: Repository<Currency>
   ) {}
-    
-  async create(createCurrencyDto: CreateCurrencyDto) {
-    const { name, code, symbol } = createCurrencyDto;
-
-    const currency = new Currency();
-    currency.name = name;
-    currency.code = code;
-    currency.symbol = symbol;
-
-    await this.currencyRepository.save(currency);
-    return currency;
-  }
 
   async findAll() {
     return await this.currencyRepository.find();
